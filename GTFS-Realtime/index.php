a2629ae39b3068feff44127edf29a8733096010d
Bradley Dice <bdice1400@gmail.com>
2013-06-02 11:40:50 -0500

    More fixes

--- GTFS-Realtime/index.php
+++ GTFS-Realtime/index.php
@@ -1,8 +1,27 @@
 <?php
-require_once 'DrSlump/Protobuf.php';
+require_once('DrSlump/Protobuf.php');
+require_once('gtfs-realtime-proto.php');
+
 \DrSlump\Protobuf::autoload();
 
-$codec = new \DrSlump\Protobuf\Codec\Json();
+$jsonFileContents = file_get_contents('realtimeData.json');
 
-echo "asdf";
+$theData = json_decode($jsonFileContents, true);
+$theMessage = $theData["message"];
+
+print_r($theMessage);
+
+$codec = new \DrSlump\Protobuf\Codec\PhpArray();
+
+$buf = new transit_realtime\FeedMessage();
+$bufHeader = new transit_realtime\FeedHeader();
+$bufEntity = new transit_realtime\FeedEntity();
+
+$bufHeader->setGtfsRealtimeVersion("1.0");
+$bufHeader->setIncrementality(0);
+$bufHeader->setTimestamp();
+
+$buf->setHeader($bufHeader);
+$buf->setEntity($bufEntity);
+
 ?>
--- ScrapingServer/index.php
+++ ScrapingServer/index.php
@@ -3,10 +3,10 @@
 	$myScraper = new KcMetroRealtimeScraper();
 	$theData = $myScraper->getData();
 	echo $theData;
-	/*$outputFile = fopen('/tmp/realtimeData.json', 'c');
+	$outputFile = fopen('../GTFS-Realtime/realtimeData.json', 'c');
 	flock($outputFile, 'LOCK_EX');
 	fwrite($outputFile, $theData);
 	flock($outputFile, 'LOCK_UN');
-	fclose($outputFile);*/
-	file_put_contents('realtimeData.json',$theData);
+	fclose($outputFile);
+	//file_put_contents('../GTFS-Realtime/realtimeData.json',$theData);
 ?>
