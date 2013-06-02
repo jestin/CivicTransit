4d22d7a28117c7473b63f8f1b7bb049b8dc97bbb
Jestin Stoffel <jestin.stoffel@gmail.com>
2013-06-02 13:28:13 -0500

    started writing out raw records

--- ScrapingServer/kcMetroRealtimeDestination.php
+++ ScrapingServer/kcMetroRealtimeDestination.php
@@ -7,6 +7,8 @@
 			if (class_exists('Mongo')) {
 				$m = new MongoClient(); // connect
 				$db = $m->selectDB("transit");
+				$record = json_decode('{ "scraperId" : "kc-ata-vehicles-realtime", "raw" : ' . $data . ' }');
+				$db->RawScrapes->save($record);
 			} else {
 				error_log('Mongo not installed');
 			}
