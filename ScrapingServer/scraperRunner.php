d05c3fd88293035ab01999d0d7ba3ccf84329c9c
Jestin Stoffel <jestin.stoffel@gmail.com>
2013-06-02 14:29:07 -0500

    added in database caching

--- ScrapingServer/scraperRunner.php
+++ ScrapingServer/scraperRunner.php
@@ -14,8 +14,25 @@
 					break;
 			}
 
+			if (class_exists('Mongo')) {
+				$m = new MongoClient(); // connect
+				$db = $m->selectDB("transit");
+				
+				$cursor = $db->RawScrapes->find(array('scraperId' => $scraperId))->sort(array('timestamp' => -1))->limit(1);
+				$curtime = time();
+				foreach($cursor as $doc) {
+					$timestamp = strtotime($doc['timestamp']);
+					$timesince = $curtime - $timestamp;
+					if(date("s",$timesince) < 15 ){
+						return json_encode($doc['raw'], JSON_PRETTY_PRINT); 
+					}
+				}
+			}
+			
+			
 			$data = $scraper->getData();
 			$destination->send($data);
+			
 			return $data;
 		}
 	}
