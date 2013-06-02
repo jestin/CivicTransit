f857df32814fd0dccc9a9427cb723cddc0fc7052
Bradley Dice <bdice1400@gmail.com>
2013-06-02 10:46:58 -0500

    Making protobuffer code

---
+++ GTFS-Realtime/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'DrSlump/Protobuf.php';
+\DrSlump\Protobuf::autoload();
+
+$codec = new \DrSlump\Protobuf\Codec\Json();
+
+echo "asdf";
+?>
--- ScrapingServer/index.php
+++ ScrapingServer/index.php
@@ -3,4 +3,10 @@
 	$myScraper = new KcMetroRealtimeScraper();
 	$theData = $myScraper->getData();
 	echo $theData;
+	/*$outputFile = fopen('/tmp/realtimeData.json', 'c');
+	flock($outputFile, 'LOCK_EX');
+	fwrite($outputFile, $theData);
+	flock($outputFile, 'LOCK_UN');
+	fclose($outputFile);*/
+	file_put_contents('realtimeData.json',$theData);
 ?>
