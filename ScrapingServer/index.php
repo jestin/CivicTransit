5ed191fee73c23cf25ae5e3affd16d775e745d0d
Bradley Dice <bdice1400@gmail.com>
2013-06-02 14:27:40 -0500

    Fixed time

--- ScrapingServer/index.php
+++ ScrapingServer/index.php
@@ -3,7 +3,7 @@
 	require_once("scraperRunner.php");
 	$runner = new scraperRunner();
 	if($_GET["loop"]==true){
-		for($i = 0; $i<60/$loopTime; $i++){
+		for($i = 0; $i<60/($i*$loopTime); $i++){
 			$theData = $runner->runScraper("kc-ata-vehicles-realtime");
 			echo $theData;
 			file_put_contents('../GTFS-Realtime/realtimeData.json',$theData);
